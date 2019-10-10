baf931009143c2b7ba2eaefed08c32ee8115921d
Vincent Mac <vincentmac@gmail.com>
2019-10-10 15:13:27 -0700

    refactor hello to use a switch and private function

--- hello/hello.go
+++ hello/hello.go
@@ -13,15 +13,19 @@ func Hello(name, language string) string {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
+	return greetingPrefix(language) + name
+}
 
-	if language == french {
-		return frenchHelloPrefix + name
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
