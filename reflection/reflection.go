737a9e3b798ad98737cda249ebe386bce1d7bff8
Vincent Mac <vincentmac@gmail.com>
2019-10-28 19:36:08 -0700

    reflection

---
+++ reflection/reflection.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"reflect"
+)
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+			// walk(val.MapIndex(key).Interface(), fn)
+		}
+	}
+
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	// fmt.Printf("val %v -> kind %v\n", val, val.Kind())
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
